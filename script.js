ee6854978b2c69c142260c3a5f73223a13b8b1a5
SmokeBellow <merzlakov@yandex.ru>
2025-05-23 18:02:51 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -177,7 +177,7 @@ const classData = {
 
 // Сброс всех экранов
 function resetScreens() {
-  document.querySelectorAll("#splash-dev, #splash-title, #main-menu, #about-screen, #class-selection, #game-screen, #travel-screen, #npc-dialog, #quests-screen, #inventory-screen")
+  document.querySelectorAll("#splash-dev, #splash-title, #main-menu, #about-screen, #class-selection, #game-screen, #travel-screen, #npc-dialog, #character-screen, #quests-screen, #inventory-screen")
     .forEach(el => {
       el.classList.remove("visible");
       el.classList.add("hidden");
